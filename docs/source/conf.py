2ab75da328f2eecb5d01a1d3b300a30397e0db23
oscarsjogren <sjogren.oscar@gmail.com>
2017-05-25 16:30:51 +0200

    added license

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -240,7 +240,7 @@ html_static_path = ['_static']
 # html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'dlwdoc'
+htmlhelp_basename = 'ezclimatedoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
