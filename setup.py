5598c7919f209322967234441efdba1424c6a0dc
Gernot Wagner <github@gwagner.com>
2019-03-12 10:25:18 -0400

    Updated info, after 3.7 conversion
    Latest model used for PNAS R&R submission

--- setup.py
+++ setup.py
@@ -9,22 +9,20 @@ else:
     read_md = lambda f: convert(f, 'rst')
 
 setup(name='ezclimate',
-      version='1.2.1b1',
+      version='2.0',
       description='EZ-Climate model',
       long_description=read_md('README.md'),
       classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: PNAS R&R-190312-gw',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 3.7'
       ],
-      keywords='EZ-climate, optimal carbon price, CO2 tax, social cost of carbon, SCC, climate pricing',
+      keywords='EZ-climate, optimal carbon price, CO2 tax, social cost of carbon, SCC, social cost of carbon dioxide, SC-CO2',
       url='http://github.com/Litterman/EZClimate',
-      author='Robert Litterman, Kent Daniel, Gernot Wagner',
+      author='Gernot Wagner, Kent Daniel, Robert Litterman',
       license='MIT',
       packages=find_packages(),
       install_requires=['numpy',],
       include_package_data=False,
       zip_safe=True
       )
-
